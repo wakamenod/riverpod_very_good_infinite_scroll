29b3f2b71c45f4c8948aac4dbdfccd77acaac488
wakamenod <wakamenod@gmail.com>
2023-10-11 07:33:25 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# riverpod_very_good_infinite_scroll
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/riverpod_very_good_infinite_scroll/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpod_very_good_infinite_scroll
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
