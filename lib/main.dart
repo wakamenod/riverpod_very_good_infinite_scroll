f9e268621a5cd744024c094e353d9165e4bc2a22
wakamenod <wakamenod@gmail.com>
2023-10-11 07:58:09 +0900

    add very_good_infinite_list

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:very_good_infinite_list/very_good_infinite_list.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() => runApp(const MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _MyAppState extends State<MyApp> {
+  var _items = <String>[];
+  var _isLoading = false;
+
+  void _fetchData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await Future.delayed(const Duration(seconds: 1));
+
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = false;
+      _items = List.generate(_items.length + 10, (i) => 'Item $i');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple Example'),
+      ),
+      body: InfiniteList(
+        itemCount: _items.length,
+        isLoading: _isLoading,
+        onFetchData: _fetchData,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, index) {
+          return ListTile(
+            dense: true,
+            title: Text(_items[index]),
+          );
+        },
       ),
     );
   }
